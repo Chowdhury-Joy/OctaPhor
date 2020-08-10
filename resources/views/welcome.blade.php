b478349df228b9a83cccf69d25fc7b206a728508
Chowdhury-Joy <62534869+Chowdhury-Joy@users.noreply.github.com>
2020-08-10 21:02:48 +0600

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,18 +5,12 @@
     @if (Route::has('login'))
     <div class="top-right links">
 
-<div class="header-image">
-    <div class="header-text">
-        <h5>WE ARE</h5>
-        <h3>CREATIVE</h3>
-        <h1>AGENCY</h1>       
-    </div>
-    <div class="header-text2">
-        <h5>GROW & NURTURE</h5>
-        <h3>THE IDEAS YOU HAVE</h3>
-        <h1>TOGETHER</h1>       
-    </div>
-</div>
+        {{-- For perticularly this page we don't need signin or register option --}}
+
+        {{-- @auth
+                <a href="{{ url('/home') }}">Home</a>
+        @else
+        <a href="{{ route('login') }}">Login</a>
 
         @if (Route::has('register'))
         <a href="{{ route('register') }}">Register</a>
